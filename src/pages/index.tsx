79608fd38fe9fb109678834a3bc022089dc270f9
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-12-01 06:12:19 +0900

    getStaticPropsで無事記事情報を取得！

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,30 @@
-import { VFC } from 'react';
-import { Header } from '../components/header';
+import Head from 'next/head';
+import MainLayout from '../layouts';
 import styles from '../styles/Home.module.scss';
 
-type LayoutProps = {
-  children: React.ReactNode;
-};
-
-export default function MainLayout({ children }): JSX.Element {
+export default function Home(props) {
+  console.log(props.topArticles);
   return (
-    <>
-      <Header />
-      <main className={styles.main}>{children}</main>
-    </>
+    <MainLayout>
+      <Head>
+        <title>Simple News</title>
+      </Head>
+    </MainLayout>
   );
 }
+
+export const getStaticProps = async () => {
+  const pageSize = 10;
+  const topRes = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=jp&pageSize=${pageSize}&apiKey=cea0809cd6484229bf2a490cde7f090b`
+  );
+  const topJson = await topRes.json();
+  const topArticles = topJson?.articles;
+
+  return {
+    props: {
+      topArticles,
+    },
+    revalidate: 60 * 10,
+  };
+};
