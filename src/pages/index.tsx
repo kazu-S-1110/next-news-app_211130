f3db9a05fff4cfdb6b214d39743e8bc6d5940416
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-12-01 12:30:33 +0900

    Articleコンポーネントを作成、記事表示ができた！

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,38 @@
-import { VFC } from 'react';
-import { Header } from '../components/header';
+import Head from 'next/head';
+import MainLayout from '../layouts';
 import styles from '../styles/Home.module.scss';
+import Article from '../components/article';
 
-type LayoutProps = {
-  children: React.ReactNode;
+const Home = (props) => {
+  console.log(props.topArticles);
+  return (
+    <MainLayout>
+      <Head>
+        <title>Simple News</title>
+      </Head>
+
+      <div className={styles.main}>
+        <Article title="headlines" articles={props.topArticles} />
+      </div>
+    </MainLayout>
+  );
 };
 
-export default function MainLayout({ children }): JSX.Element {
-  return (
-    <>
-      <Header />
-      <main className={styles.main}>{children}</main>
-    </>
+export default Home;
+
+export const getStaticProps = async () => {
+  // NewsAPIのトップ記事の情報を取得
+  const pageSize = 10;
+  const topRes = await fetch(
+    `https://newsapi.org/v2/top-headlines?country=jp&pageSize=${pageSize}&apiKey=${process.env.NEXT_PUBLIC_NEWS_APIKEY}`
   );
-}
+  const topJson = await topRes.json();
+  const topArticles = topJson?.articles;
+
+  return {
+    props: {
+      topArticles,
+    },
+    revalidate: 60 * 10,
+  };
+};
