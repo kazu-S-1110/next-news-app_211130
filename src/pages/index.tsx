9e2da5f9fc4ff469618e9440f51d32292cd4ba0f
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-12-02 12:41:44 +0900

    天気情報を表示するコンポーネントを実装。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import MainLayout from '../layouts';
 import styles from '../styles/Home.module.scss';
 import Article from '../components/article';
 import Nav from '../components/nav';
+import WeatherNews from '../components/weather-news';
 
 const Home = (props) => {
   return (
@@ -21,6 +22,9 @@ const Home = (props) => {
         <div className={styles.main}>
           <Article title="headlines" articles={props.topArticles} />
         </div>
+        <div className={styles.aside}>
+          <WeatherNews weatherNews={props.weatherNews} />
+        </div>
       </div>
     </MainLayout>
   );
@@ -37,9 +41,18 @@ export const getStaticProps = async () => {
   const topJson = await topRes.json();
   const topArticles = topJson?.articles;
 
+  const lat = 35.4122;
+  const lon = 139.413;
+  const exclude = 'hourly,minutely';
+  const weatherRes = await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=${exclude}&appid=${process.env.NEXT_PUBLIC_WEATHER_APIKEY}`
+  );
+  const weatherJson = await weatherRes.json();
+  const weatherNews = weatherJson;
   return {
     props: {
       topArticles,
+      weatherNews,
     },
     revalidate: 60 * 10,
   };
